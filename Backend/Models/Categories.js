c5cf93e8f77d3ead9f0783c538e44efd0d5ef440
Mayank Sati <mmayanksati@gmail.com>
2020-04-25 04:20:04 +0530

    backend

---
+++ Backend/Models/Categories.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const categoriesSchema = new Schema({
+        categoryName: { type: String, unique: true, required: true, trim: true },
+        products: [{type: Schema.Types.ObjectId, ref: "Products"}],
+        subCategories: [{type: Schema.Types.ObjectId, ref: "SubCategories"}]
+});
+
+const Categories = mongoose.model("Categories", categoriesSchema);
+
+module.exports = Categories;
---
+++ Backend/Models/SubSubCategories.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const subSubCategoriesSchema = new Schema({
+        subSubCategoryName: { type: String, unique: true, required: true, trim: true },
+        Products: [{type: Schema.Types.ObjectId, ref: "Products"}]		
+});
+
+const SubSubCategories = mongoose.model("SubSubCategories", subSubCategoriesSchema);
+
+module.exports = SubSubCategories;
